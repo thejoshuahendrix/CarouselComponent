79ffcfcd91b88a5b86c2228839ef84bd9afeca0c
Joshua Hendrix <56802866+thejoshuahendrix@users.noreply.github.com>
2021-12-26 10:51:10 -0500

    added cards (#2)

---
+++ src/components/Card.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+    return (
+        <div style={{ textAlign: "center" }}>
+            <h2>Title</h2>
+            <p>Description</p>
+        </div>
+    )
+}
+
+export default Card
